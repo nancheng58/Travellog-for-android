272cfb82fc7d8a6742f16f4622a3b7dc0fc4a3ef
nancheng58 <nancheng58@qq.com>
2021-02-25 18:36:25 +0800

    更改模型名

---
+++ app/src/main/java/com/code/travellog/core/data/pojo/BasePojo.java
@@ -0,0 +1,12 @@
+package com.code.travellog.core.data.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @deprecated : 响应信号基类
+ * @date: 2021/2/25
+ */
+public class BasePojo implements Serializable {
+    public int code = -1;
+    public String msg = null ;
+}
